89cbfb6bfbc83df094fc947b44d83898e2beebba
luislopez23 <llopezgall@student.rccd.edu>
2024-03-28 13:31:16 -0700

    Pushing file

---
+++ Lab/HelloWorld/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: 2685529
+ *
+ * Created on March 28, 2024, 1:25 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout << "Hello World" << endl;
+    
+    return 0;
+}
+
